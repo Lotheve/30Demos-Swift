f4b4e3dd43e7dea40170e175b1edbf7afed695bd
Lotheve <gaofeng-7171@163.com>
2017-11-04 23:17:13 +0800

    添加渐变色demo

---
+++ LTVSwift/LTVSwift/5.渐变色/GraduateColorController.swift
@@ -0,0 +1,23 @@
+//
+//  GraduateColorController.swift
+//  LTVSwift
+//
+//  Created by 卢旭峰 on 2017/11/4.
+//  Copyright © 2017年 Lotheve. All rights reserved.
+//
+
+import UIKit
+
+class GraduateColorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
